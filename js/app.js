1f7a8095736a6aa0515602b261f0f63c954cbbdf
Rafal Kapusta <rfkapusta@gmail.com>
2019-10-20 22:42:54 +0200

    added js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,26 @@
    <button class="close">Close</button>
  </div>
  */
+let allImg = document.querySelectorAll('img');
+let body = document.querySelector('body');
+
+// console.log(allLi, body);
+
+allImg.forEach(function(img) {
+    img.addEventListener('click', function (e) {
+        let imgSrc = this.getAttribute('src');
+        let fullScreen = document.createElement('div');
+        fullScreen.className = 'fullScreen';
+        fullScreen.innerHTML += `<img src=${imgSrc}>`;
+        fullScreen.innerHTML += `<button class="close">Close</button>`;
+        body.appendChild(fullScreen);
+        let button = document.querySelector('button');
+        button.addEventListener('click', function(e){
+            body.removeChild(fullScreen);
+        })
+    })
+})
+
+
+
+
